c40a82cf18d18b5f1a9edd68e35b21999ad31d82
k07g <korenaga.makoto@ea-sys.jp>
2026-02-04 12:09:50 +0900

    Add fetch error

---
+++ 03/fetch_error.js
@@ -0,0 +1,13 @@
+fetch("https://wings.msn.to/tmp/it/nothing.php")
+  .then((response) => {
+    // 応答が成功(ok)であれば結果を取得
+    if (response.ok) {
+      return response.text();
+    }
+    // 成功(ok)でなければ、ステータスメッセージを例外としてスロー
+    throw new Error(response.statusText);
+  })
+  // 正常終了時は結果をログ出力
+  .then((text) => console.log(text))
+  // 例外発生時は例外メッセージをログ出力
+  .catch((err) => console.error(err.message));
