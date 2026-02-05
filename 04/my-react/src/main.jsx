c1f1ffdfbf2eaba0d498121d309405965940ada2
k07g <korenaga.makoto@ea-sys.jp>
2026-02-06 08:16:46 +0900

    AppClassの追加

--- 04/my-react/src/main.jsx
+++ 04/my-react/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
+import AppClass from './AppClass.jsx'
 
+/*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+*/
+/*
+const root = createRoot(document.getElementById('root'));
+setInterval(() => {
+  root.render(
+    <p>現在時刻: {(new Date()).toLocaleString()}</p>
+  );
+}, 1000);
+*/
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AppClass />
+  </StrictMode>
 )
