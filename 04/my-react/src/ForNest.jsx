5c54b6dab9d5a8183b997540f1cab76481b4dea3
k07g <korenaga.makoto@ea-sys.jp>
2026-02-06 11:35:59 +0900

    Add ForNest

---
+++ 04/my-react/src/ForNest.jsx
@@ -0,0 +1,9 @@
+import ForItem from "./ForItem";
+
+export default function ForNest({src}) {
+    return (
+        <dl>
+            {src.map(elem => <ForItem book={elem} key={elem.isbn} />)}
+        </dl>
+    );
+}
