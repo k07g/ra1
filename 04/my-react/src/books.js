08f4ed9c090efc9fd323af579e6fdb274781236b
k07g <155704074+k07g@users.noreply.github.com>
2026-02-06 12:14:33 +0900

    Add books (#32)

--- 04/my-react/src/books.js
+++ 04/my-react/src/books.js
@@ -1,10 +1,38 @@
 const books = [
-    {
-        isbn: '978-4-8156-1336-5',
-        title: 'これからはじめるVus.js 3実践入門',
-        price: 3740,
-        summary: 'JavaScriptフレームワーク「Vue.js」について・・・',
-        download: true,
-    },
+  {
+    isbn: "978-4-8156-1336-5",
+    title: "これからはじめるVus.js 3実践入門",
+    price: 3740,
+    summary: "JavaScriptフレームワーク「Vue.js」について・・・",
+    download: true,
+  },
+  {
+    isbn: "978-4-7981-8949-9",
+    title: "独習Python 第2版",
+    price: 3608,
+    summary: "基礎からきっちり身に付くPython入門書",
+    download: true,
+  },
+  {
+    isbn: "978-4-7981-8055-7",
+    title: "独習ASP.NET Core",
+    price: 4290,
+    summary: "クロスプラットフォームでWebアプリ開発",
+    download: true,
+  },
+  {
+    isbn: "978-4-7981-8094-6",
+    title: "独習Java 第6版",
+    price: 3278,
+    summary: "「標準教科書」がJava 21対応の最新版で登場！",
+    download: true,
+  },
+  {
+    isbn: "978-4-7981-7556-0",
+    title: "独習C# 第5版",
+    price: 4180,
+    summary: "C#言語の独学に最適な教科書",
+    download: true,
+  },
 ];
 export default books;
