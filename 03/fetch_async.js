2cb83eae63f7f5bc6f95d46b7237264e91adf153
k07g <korenaga.makoto@ea-sys.jp>
2026-02-06 13:49:24 +0900

    ファイルのフォーマット

--- 03/fetch_async.js
+++ 03/fetch_async.js
@@ -1,8 +1,10 @@
 async function fetchData() {
-    const response = await fetch('https://wings.msn.to/tmp/it/fetch.php');
-    if (!response.ok) {throw new Error(response.statusText);    }
-    const text = await response.text();
-    console.log(text);
+  const response = await fetch("https://wings.msn.to/tmp/it/fetch.php");
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const text = await response.text();
+  console.log(text);
 }
 
 fetchData();
