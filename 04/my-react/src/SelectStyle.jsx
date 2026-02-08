270595ace3e196887839378e9d791acbb5274b12
k07g <korenaga.makoto@ea-sys.jp>
2026-02-08 15:16:50 +0900

    Update SelectStyle by cn

--- 04/my-react/src/SelectStyle.jsx
+++ 04/my-react/src/SelectStyle.jsx
@@ -1,9 +1,10 @@
 import './SelectStyle.css';
+import cn from 'classnames';
 
 export default function SelectStyle({ mode }) {
   return (
     // mode属性に応じてスタイルクラスを切り替え
-    <div className={`box ${mode === "light" ? "light" : "dark"}`}>
+    <div className={cn('box', mode === 'light' ? 'light' : 'dark')}>
       こんにちは、世界！
     </div>
   );
