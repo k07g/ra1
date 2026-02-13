887429e883c0a00f478102ee4b170340b87c9de3
k07g <korenaga.makoto@ea-sys.jp>
2026-02-13 11:20:03 +0900

    Add EventKey

---
+++ 04/my-react/src/EventKey.jsx
@@ -0,0 +1,17 @@
+export default function EventKey() {
+  // Ctrl + q でヘルプメッセージを表示
+  const handleKey = (e) => {
+    if (e.ctrlKey && e.key === "q") {
+      alert("名前は20文字以内で入力してください。");
+    }
+  };
+
+  return (
+    <form>
+      <label>
+        名前:
+        <input type="text" size="20" onKeyDown={handleKey} />
+      </label>
+    </form>
+  );
+}
